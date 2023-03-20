edfae11eb2d08f8c0a25b4f37a4589d829e475cc
Abraão Costa <abraaocrl@hotmail.com>
2023-03-20 18:43:59 -0300

    feat - Tratamento de fluxo de login

---
+++ Models/Util/HttpRequestExtensions.cs
@@ -0,0 +1,27 @@
+﻿namespace TCC_WEB.Models.Util
+{
+    public static class HttpRequestExtensions
+    {
+        public static string? BaseUrl(this HttpRequest request)
+        {
+            if (request == null) throw new ArgumentNullException(nameof(request));
+
+            var uriBuilder = new UriBuilder(request.Scheme,request.Host.Host,request.Host.Port ?? -1);
+            if(uriBuilder.Uri.IsDefaultPort)
+            {
+                uriBuilder.Port = -1;
+            }
+
+            return uriBuilder.Uri.AbsoluteUri;
+        }
+
+        public static bool IsReleaseBuild()
+        {
+            #if DEBUG
+                return false;
+            #else
+                return true;
+            #endif
+        }
+    }
+}
