5594745818cdaaf88cc7896935f37fef96456488
Abraão Costa <abraaocrl@hotmail.com>
2023-06-01 23:29:18 -0300

    Tela de rotas wip

--- Pages/admin/Rotas.cshtml.cs
+++ Pages/admin/Rotas.cshtml.cs
@@ -1,12 +1,22 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Routing;
 
-namespace TCC_WEB.Pages.Rotas
+namespace TCC_WEB.Pages.admin
 {
     public class RotasModel : PageModel
     {
+        private LinkGenerator linkGenerator;
+        public string UriRotaEdicaoPage { get; set; }
+
+        public RotasModel(LinkGenerator linkGenerator)
+        {
+            this.linkGenerator = linkGenerator;
+        }
+
         public void OnGet()
         {
+            this.UriRotaEdicaoPage = linkGenerator.GetPathByPage(this.HttpContext, page: "Rotas_Edit", values: new { id = 0 });
         }
     }
 }
--- Pages/admin/Rotas_Edit.cshtml.cs
+++ Pages/admin/Rotas_Edit.cshtml.cs
@@ -1,9 +1,9 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace TCC_WEB.Pages
+namespace TCC_WEB.Pages.admin
 {
-    public class Carros_EditModel : PageModel
+    public class Rotas_EditModel : PageModel
     {
         public void OnGet()
         {
