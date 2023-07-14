4094167b69ced17aa34f4b98ef7ea103eef8ef7b
Abraão Costa <abraaocrl@hotmail.com>
2023-07-14 11:47:27 -0300

    Urls da API em produção

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -5,6 +5,13 @@ namespace TCC_WEB.Pages
 {
     public class IndexModel : PageModel
     {
+        private readonly IConfiguration _configuration;
+
+        public IndexModel(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
         public void OnGet()
         {
         }
